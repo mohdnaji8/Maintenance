f80dfba785c03f04ac412b4ff25e94cd43f7255c
mohdnaji8 <mnaji8194@gmail.com>
2022-08-17 10:02:00 +0300

    create order

--- resources/views/maintenance/orders/create.blade.php
+++ resources/views/maintenance/orders/create.blade.php
@@ -28,6 +28,20 @@
                 </div>
             </div>
         </div>
+        <div class="row" >
+            <div class="form-group">
+                <label class="col-md-3 control-label">اسم الحرفة</label>
+                <div class="col-md-3">
+                    <input type="text" name="job_name" class="form-control" placeholder="Enter text">
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-md-3 control-label">الضريبة السنوية</label>
+                <div class="col-md-3">
+                    <input type="text" name="annual_tax" class="form-control" placeholder="Enter text">
+                </div>
+            </div>
+        </div>
         <div class="form-group">
             <button type="submit" id="add_job" class="btn btn-primary font-weight-bold">حفظ </button>
         </div>
